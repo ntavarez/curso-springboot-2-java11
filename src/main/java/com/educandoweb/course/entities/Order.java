a6cc1dc04a6e9582ffa5bf2cd8326d66f54fc11c
Natalia Tavares <natalia.h.tavares@gmail.com>
2020-05-13 22:13:21 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -16,7 +16,6 @@ import javax.persistence.Table;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order")
